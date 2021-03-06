352057199a90968a113a9e9a3c78f43dfcbe2835
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-03-06 23:21:11 +0300

    ➕ Add "toggleTheme" API

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [unreleased] - TODO: release date here
+
+- Add `toggleTheme`
+
 ## [0.0.1] - 6 Mar, 2021
 
 - Initial Release 🚀
--- lib/src/common/theme_mode_builder_config.dart
+++ lib/src/common/theme_mode_builder_config.dart
@@ -1,7 +1,23 @@
+import 'package:hive/hive.dart';
 import 'package:theme_mode_builder/src/services/hive_storage_service.dart';
 
 class ThemeModeBuilderConfig {
   static Future<void> ensureInitialized() async {
     await HiveStorageService().init();
+  }
+
+  static Future<void> toggleTheme() async {
+    final Box themeBox = HiveStorageService().getBox(
+      boxName: "theme_box",
+    );
+
+    final bool isDarkTheme = themeBox.get(
+      "is_dark_key",
+    );
+
+    await themeBox.put(
+      "is_dark_key",
+      !isDarkTheme,
+    );
   }
 }
